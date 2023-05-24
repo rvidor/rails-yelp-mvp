503d40ce8ff4df11a0bd699825fd933f1b99651a
Richard Vidor <richardvidor1@gmail.com>
2023-05-24 17:03:52 +0100

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: '02035467890', category: "indian"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: '02034568753', category: "italian"}
+mildreds = {name: 'Mildreds', address: 'Dalston', phone_number: '020352865558'}
+
+[dishoom, pizza_east, mildreds].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
